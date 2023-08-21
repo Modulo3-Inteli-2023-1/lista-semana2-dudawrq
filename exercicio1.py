6702ec959b994ac763c3a6dfbfcbc00383a90cf2
dudawrq <eduarda.souza@sou.inteli.edu.br>
2023-08-21 02:17:03 -0300

    resolução exercicios semana 2

--- exercicio1.py
+++ exercicio1.py
@@ -5,7 +5,14 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-
+def conta_palavras_unicas(frase):
+    frase = frase.lower().replace('.', '').replace(',', '').replace('!', '').replace('?', '').replace('-', '')
+    palavras = frase.split()
+    palavras_unicas = set()
+    for palavra in palavras:
+        palavras_unicas.add(palavra)
+    
+    return len(palavras_unicas)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -5,7 +5,24 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def is_anagram(palavra1, palavra2):
 
+    palavra1 = palavra1.replace(" ", "").lower()
+    palavra2 = palavra2.replace(" ", "").lower()
+
+    if len(palavra1) != len(palavra2):
+        return False
+    
+    contar_palavra1 = {}
+    contar_palavra2 = {}
+    
+    for i in palavra1:
+        contar_palavra1[i] = contar_palavra1.get(i, 0) + 1
+    
+    for i in palavra2:
+        contar_palavra2[i] = contar_palavra2.get(i, 0) + 1
+    
+    return contar_palavra1 == contar_palavra2
 
 
 
